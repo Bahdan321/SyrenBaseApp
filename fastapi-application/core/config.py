7a77704168d0e521ed397a8cbd1c27da99b442e5
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-05-30 20:09:51 +0200

    adding db to config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,25 +1,35 @@
-from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
 
 
 class RunConfig(BaseModel):
-        host: str = "0.0.0.0"
-        port: int = 8000
+    host: str = "0.0.0.0"
+    port: int = 8000
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    pool_size: int = 50,
-    max_overflow: int = 10,
-
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    db: DatabaseConfig = DatabaseConfig()
+    db: DatabaseConfig
 
 settings = Settings()
+print(settings.db.url)
